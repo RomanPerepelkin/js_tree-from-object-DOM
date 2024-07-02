43b74ed2f25b2c5f67a9397e56ad0ae74f83ee4f
Roman Perepelkin <perepelkinrs@gmail.com>
2024-07-02 15:14:21 +0300

    make build

---
+++ index.ff418f26.js
@@ -0,0 +1,2 @@
+!function e(n,r){var t=document.createElement("ul");for(var a in r){var l=document.createElement("li");l.innerText=a,t.appendChild(l),n.appendChild(t),Object.keys(r[a]).length>0&&e(l,r[a])}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.ff418f26.js.map
